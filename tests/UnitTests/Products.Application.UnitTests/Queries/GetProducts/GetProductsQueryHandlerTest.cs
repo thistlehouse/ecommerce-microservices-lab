807e5ceb268e1b11724089358bc18dc51a97c166
thistlehouse <fernando2802@gmail.com>
2025-12-08 01:22:35 -0300

    added test GetProducts
    chore: moved files to folder queries

---
+++ tests/UnitTests/Products.Application.UnitTests/Queries/GetProducts/GetProductsQueryHandlerTest.cs
@@ -0,0 +1,67 @@
+using ErrorOr;
+using FluentAssertions;
+using Moq;
+using Products.Application.Common.Abstractions;
+using Products.Application.Queries.GetProducts;
+using Products.Domain.Products;
+
+namespace Products.Application.UnitTests.Queries.GetProducts;
+
+public sealed class GetProductsQueryHandlerTest
+{
+    private readonly GetProductsQueryHandler _handler;
+    private readonly Mock<IProductRepository> _mockProductRepository;
+
+    public GetProductsQueryHandlerTest()
+    {
+        _mockProductRepository = new Mock<IProductRepository>();
+        _handler = new GetProductsQueryHandler(_mockProductRepository.Object);
+    }
+
+    [Fact]
+    public async Task HandleGetProductsQuery_WhenProductsExist_ShouldReturnProducts()
+    {
+        IEnumerable<Product> products = Enumerable.Range(0, 100)
+            .Select(idx => Product.Create(
+                $"ProductName_{idx}",
+                $"ProductDescription_{idx}",
+                new Random().Next(5, 1000)));
+
+        GetProductsQuery query = new(1, 10);
+
+        _mockProductRepository.Setup(m => m.GetProducts(query.Page, query.PageSize, out It.Ref<int>.IsAny))
+            .Callback((int page, int pageSize, out int totalItems) =>
+            {
+                totalItems = products.Count();
+            })
+            .Returns(products);
+
+        ErrorOr<GetProductsResult> result = await _handler.Handle(
+            query,
+            default);
+
+        result.IsError.Should().BeFalse();
+        result.Value.Products.Should().NotBeNull();
+        result.Value.HasPreviousPage.Should().BeFalse();
+        result.Value.HasNextPage.Should().BeTrue();
+        result.Value.TotalItems.Should().Be(result.Value.Products.Count);
+        result.Value.TotalPages.Should().Be((int)Math.Ceiling((double)result.Value.TotalItems / result.Value.PageSize));
+    }
+
+    [Fact]
+    public async Task HandleGetProductsQuery_WhenProductsDoesNotExist_ShouldReturnNotFound()
+    {
+        int totalItems = 0;
+        GetProductsQuery query = new(1, 10);
+
+        _mockProductRepository.Setup(m => m.GetProducts(query.Page, query.PageSize, out totalItems))
+            .Returns([]);
+
+        ErrorOr<GetProductsResult> result = await _handler.Handle(
+            query,
+            default);
+
+        result.IsError.Should().BeTrue();
+        result.FirstError.Code.Should().Be("ProductsNotFound");
+    }
+}

2d9537c7d1f48bf1c5c117db34f5e7ce0398bbea
thistlehouse <fernando2802@gmail.com>
2025-12-23 12:23:06 -0300

    refactor: register endpoint
    feat: added api errors handling

--- src/Users/Users.Api/Endpoints/RegisterUser.cs
+++ src/Users/Users.Api/Endpoints/RegisterUser.cs
@@ -1,3 +1,9 @@
+using ErrorOr;
+using MediatR;
+using Users.Api.Errors;
+using Users.Application.Authentication.Command.RegisterUser;
+using Users.Application.Authentication.Common;
+
 namespace Users.Api.Endpoints;
 
 public sealed class RegisterUser : IEndpoint
@@ -6,7 +12,20 @@ public sealed class RegisterUser : IEndpoint
     {
         app.MapPost(
             "/register",
-            (RegisterUserRequest request) => $"Hello {request.FirstName}");
+            async (ISender sender, RegisterUserRequest request) =>
+        {
+            RegisterUserCommand command = new(
+                request.FirstName,
+                request.LastName,
+                request.Email,
+                request.Password);
+
+            ErrorOr<AuthenticationResult> result = await sender.Send(command, default);
+
+            return result.Match(
+                auth => Results.Created("/register", auth),
+                errors => ApiErrors.Problem(errors));
+        });
     }
 
     private record RegisterUserRequest(

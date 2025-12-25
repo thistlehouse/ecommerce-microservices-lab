fd80dce575bc520f109ed9a558f6179602bc00a6
thistlehouse <fernando2802@gmail.com>
2025-12-24 22:54:32 -0300

    refactor: removed jwt authentication

--- src/Users/Users.Infrastructure/InfrastructureConfiguration.cs
+++ src/Users/Users.Infrastructure/InfrastructureConfiguration.cs
@@ -43,18 +43,6 @@ public static class InfrastructureConfiguration
         services.AddSingleton(Options.Create(permissionSettings));
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
 
-        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = jwtSettings.Issuer,
-                ValidAudience = jwtSettings.Audience,
-                IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(jwtSettings.Secret)),
-            });
-
         return services;
     }
 
